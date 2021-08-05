d4018aa60a7538a8b593adf4be06a3f180dc2634
Alexandr <torasov.alexandr@gmail.com>
2021-08-05 21:23:06 +0300

    hw3Done

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-//обернул в фукцию
-function task1() {
-  // объявил переменную до блока с циклом для возможности использовать ее на этом уровне далее
-  let temperatireCell;
-  do {
-    // получаем от пользователя строку и переводим ее в число
-    temperatireCell = +prompt(
-      "Введите температу в грудусах цельсия (число без знаков)"
-    );
-    // если строку не удается перевести в число повторяем запрос
-    // для отрицательных значений делать не стал
-  } while (temperatireCell !== temperatireCell);
-
-  // выводим темп. в градусах F
-  alert(`Температура в градусах F ${1.8 * temperatireCell + 32}`);
-}
-
-// task1();
-function hw1() {
-  // все по тексту задания только поменял местами порядок объявления anmin и name
-  let name = "Василий",
-    admin = name;
-
-  console.log(admin);
-
-  console.log(10 + 10 + "10");
-  // 10+10 = 20 + "string"=> 20"string"=> 2010 вычисления идут слева на права  (приоритет одинаковый)
-  console.log(10 + "10" + 10); //аналогично string+ number + string =string_number_string
-  console.log(10 + 10 + +"10"); // приоритет у преобразования (+"10") выше чем у + сначала строка приводится к числу затем происходит сложение слева на право (аналогично уножению в математике)
-  console.log(10 / -""); // пустую строку js определяет как 0 число разделить на 0 => бесконечность
-  console.log(10 / +"2,5"); // 2,5 переводиться в NaN number/NaN == NaN
-}
-
-// hw1();
-
 function hw2() {
   const task1 = () => {
     let a = 1,
@@ -123,5 +88,95 @@ function hw2() {
 
   task6();
 }
+// hw2();
 
-hw2();
+function hw3() {
+  const task1 = () => {
+    for (let i = 0; i <= 10; i++) {
+      if (i === 0) {
+        console.log(`${i}-это ноль`);
+      } else if (i % 2 == 0) {
+        console.log(`${i}-четное число`);
+      } else {
+        console.log(`${i}- нечетное число`);
+      }
+    }
+  };
+  task1();
+
+  const task2 = () => {
+    const post = {
+      author: "John",
+      pastId: 23,
+      comments: [
+        {
+          userId: 10,
+          userName: "Alex",
+          text: "lorem ipsum",
+          raing: { likes: 10, dislikes: 2 },
+        },
+        {
+          userId: 5,
+          userName: "Jane",
+          text: "lorem ipsum 2",
+          raing: { likes: 3, dislikes: 1 },
+        },
+      ],
+    };
+
+    console.log(post.author);
+    console.log(post.comments[0].raing.dislikes);
+    console.log(post.comments[1].text);
+  };
+
+  task2();
+
+  const task3 = () => {
+    const product = [
+      { id: 3, price: 200 },
+      { id: 4, price: 900 },
+      { id: 1, price: 1000 },
+    ];
+
+    product.map((item) => (item.price *= 0.85));
+    console.log(product);
+  };
+  task3();
+
+  const task4 = () => {
+    const product = [
+      { id: 3, price: 127, photos: ["1.jpg", "2.jpg"] },
+      { id: 5, price: 499, photos: [] },
+      { id: 10, price: 26, photos: ["3.jpg"] },
+      { id: 8, price: 78, photos: [] },
+    ];
+
+    console.log(product.filter((number) => number.photos.length > 0));
+    console.log("");
+    console.log(product.sort((a, b) => a.price - b.price));
+  };
+  task4();
+
+  const task5 = () => {
+    for (
+      let i = {
+        ref: 0,
+        lef() {
+          console.log(this.ref);
+          this.ref++;
+        },
+      };
+      i.ref < 10;
+      i.lef()
+    ) {}
+  };
+  task5();
+
+  const task6 = (h) => {
+    for (let i = "#"; i.length < h + 1; i += "#") {
+      console.log(i);
+    }
+  };
+  task6(20);
+}
+w3();
